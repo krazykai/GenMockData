770bc4af7956902ce21a7bc37b2d627907bf5114
Austin Chou <austinchoukai@gmail.com>
2024-10-16 11:43:26 +0800

    1. Add a comment to the 'GenMockDataString' API.  2. Hide 'GenMockDataFile' API

--- Controllers/GenMockController.cs
+++ Controllers/GenMockController.cs
@@ -15,6 +15,11 @@ namespace GenMockData.Controllers
             _genMockService = genMockDataService;
         }
 
+        /// <summary>
+        /// Generate mock data string
+        /// </summary>
+        /// <param name="genMockRequest"></param>
+        /// <returns> string </returns>
         [HttpPost]
         [Route("dataString")]
         public async Task<ActionResult<string>> GenMockDataString([FromBody] GenMockRequest genMockRequest)
@@ -39,30 +44,30 @@ namespace GenMockData.Controllers
             }
         }
 
-        [HttpPost]
-        [Route("dataFile")]
-        public async Task<ActionResult<IFormFile>> GenMockDataFile([FromBody] GenMockRequest genMockRequest)
-        {
-            try
-            {
-                if (genMockRequest.responseFormat.ToLower() == "sql")
-                {
-                    if (string.IsNullOrEmpty(genMockRequest.tableName) == true)
-                    {
-                        return BadRequest("tableName is required.");
-                    }
-                }
+        //[HttpPost]
+        //[Route("dataFile")]
+        //public async Task<ActionResult<IFormFile>> GenMockDataFile([FromBody] GenMockRequest genMockRequest)
+        //{
+        //    try
+        //    {
+        //        if (genMockRequest.responseFormat.ToLower() == "sql")
+        //        {
+        //            if (string.IsNullOrEmpty(genMockRequest.tableName) == true)
+        //            {
+        //                return BadRequest("tableName is required.");
+        //            }
+        //        }
 
-                string mockDataString = await _genMockService.GenMock(genMockRequest);
+        //        string mockDataString = await _genMockService.GenMock(genMockRequest);
 
-                ActionResult mockDataFile = await _genMockService.ParseMockDataStringToFile(mockDataString, genMockRequest.responseFormat);
+        //        ActionResult mockDataFile = await _genMockService.ParseMockDataStringToFile(mockDataString, genMockRequest.responseFormat);
 
-                return mockDataFile;
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex);
-            }
-        }
+        //        return mockDataFile;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex);
+        //    }
+        //}
     }
 }
