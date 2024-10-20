270f06ff9c35cb0a70ec7a895c53858893ab5723
Kai Chou <austinchoukai@gmail.com>
2024-10-20 11:41:56 +0800

    remove the route of Chat API.

--- Controllers/OpenAIController.cs
+++ Controllers/OpenAIController.cs
@@ -27,7 +27,6 @@ namespace GenMockData.Controllers
         /// <param name="content"></param>
         /// <returns></returns>
         [HttpPost]
-        [Route("OpenAI")]
         [SwaggerOperation(Summary = "Chat with OpenAI API", Description = "Chat with OpenAI API")]
         public async Task<IActionResult> Chat([FromBody] string content)
         {
